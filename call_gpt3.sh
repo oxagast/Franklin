60018e397d1598421324e80efb280828ae2339d7
Marshall Whittaker <marshall@oxagast.org>
2022-12-29 12:04:48 -0500

    Added soft and hard limit vars.

--- call_gpt3.sh
+++ call_gpt3.sh
@@ -1,6 +1,7 @@
 SAY="$1"
 APIKEY="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"
 echo "${SAY}" >> gpt3.log;
-LEN="65";
-RES=$(curl -s https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer ${APIKEY}" -d "{\"model\": \"text-davinci-003\",\"prompt\": \"${SAY}\",\"temperature\": 0.8,\"max_tokens\": ${LEN},\"top_p\": 1,\"frequency_penalty\": 0,\"presence_penalty\": 0}" | jq -r '.choices[].text')
-echo ${RES} | sed -e 's|\\n||g' | sed -e 's/[^\.]$/.../'  | cut -c -400 | tee -a gpt3.log
+HARDL="-400"
+SOFTL="65";
+RES=$(curl -s https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer ${APIKEY}" -d "{\"model\": \"text-davinci-003\",\"prompt\": \"${SAY}\",\"temperature\": 0.8,\"max_tokens\": ${SOFTL},\"top_p\": 1,\"frequency_penalty\": 0,\"presence_penalty\": 0}" | jq -r '.choices[].text')
+echo ${RES} | sed -e 's|\\n||g' | sed -e 's/[^\.]$/.../'  | cut -c ${HARDL} | tee -a gpt3.log
