f3fdc9797ed7f8a0d6767b59e24d6067e8d30faf
marshall whittaker <marshall@oxagast.org>
2023-01-03 00:37:17 +0000

    some commenting

--- call_gpt3.sh
+++ call_gpt3.sh
@@ -7,16 +7,16 @@ SOFTL="120"                        # soft limit (in words)
 SAY="$1"
 WHO="$2"
 
-
-
-
 function call_api () {
   APIKEY=$(<api.key)
   if [[ $(grep ${WHO} block.list | wc -l) -eq 0 ]]; then
     echo "${WHO}: ${SAY}" >>gpt3.log
     curl -s https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer ${APIKEY}" -d "{\"model\": \"text-davinci-003\",\"prompt\": \"${SAY}\",\"temperature\": 0.8,\"max_tokens\": ${SOFTL},\"top_p\": 1,\"frequency_penalty\": 0,\"presence_penalty\": 0}" >res.txt
+    # sanitizes input
     cat res.txt | tr -d "\'" | tr -d "\`" | tr -d "\;" | tr -d '\&' >sanitized.txt
+    # json extraction and formatting
     cat sanitized.txt | jq -r '.choices[].text' | sed -e 's|\\n||g' | tr -d '\n' >/var/www/html/said/${CALLR}
+    # add it to the webserver dir and say/log its response
     SAID=$(cat /var/www/html/said/${CALLR} | sed -e 's/[^\.]$/.../' | cut -c ${HARDL})
     echo "$SAID ${SITE}/said/${CALLR}" | tee -a gpt3.log
   else
@@ -24,13 +24,14 @@ function call_api () {
   fi
 }
 
-
-# random for the end of the url, maybe add collision protection?
+# random for the end of the url
 while [[ $CALLED -eq 0 ]]; do
   CALLR=$(echo ${RANDOM} | md5sum | cut -c -6)
   CALLED=0
+  # test to see if a file is already there by that name
   if test ! -f "/var/www/html/said/${CALLR}"; then
     CALLED=1
+    # the main api calling function
     call_api;
     break;
   else
