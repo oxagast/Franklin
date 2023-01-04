d921435117f3678cd0c094bcea47285e82de5ac1
marshall whittaker <marshall@oxagast.org>
2023-01-04 07:10:03 +0000

    Fixed collisons on md5 by checksumming the text itself.

--- call_gpt3.sh
+++ call_gpt3.sh
@@ -3,7 +3,7 @@
 
 
 SITE="https://gpt3.oxasploits.com" # where to host the overrun
-OUT_PTH="./"
+OUT_PTH="/var/www/html/"
 HARDL="-350"                       # hard limit (in bytes)
 SOFTL="120"                        # soft limit (in words)
 MODEL="text-davinci-003"
@@ -27,27 +27,18 @@ function call_api () {
       ${SOFTL},\"top_p\": 1,\"frequency_penalty\": 0,\"presence_penalty\": 0}" >res.txt
     # sanitizes input
     cat res.txt | tr -d "\'" | tr -d "\`" | tr -d "\;" | tr -d '\&' >sanitized.txt
+    CALLR=$(md5sum < sanitized.txt | cut -c -8)
     # json extraction and formatting
-    cat sanitized.txt | jq -r '.choices[].text' | sed -e 's|\\n||g' | tr -d '\n' >${OUT_PTH}/said/${CALLR}
+    echo "${SAY}" >${OUT_PTH}/said/${CALLR}
+    echo >>${OUT_PTH}/said/${CALLR}
+    OUT_TXT=$(cat sanitized.txt | jq -r '.choices[].text' | sed -e 's|\\n||g' | tr -d '\n')
+    echo "${OUT_TXT}" >>${OUT_PTH}/said/${CALLR}
     # add it to the webserver dir and say/log its response
-    SAID=$(cat ${OUT_PTH}/said/${CALLR} | sed -e 's/[^\.]$/.../' | cut -c ${HARDL})
-    echo "$SAID ${SITE}/said/${CALLR}" | tee -a gpt3.log
+    SAID=$(echo "${OUT_TXT}" | sed -e 's/[^\.]$/.../' | cut -c ${HARDL})
+    echo "${SAID} ${SITE}/said/${CALLR}" | tee -a gpt3.log
   else
     echo "You cannot use that command."
   fi
 }
 
-# random for the end of the url
-while [[ $CALLED -eq 0 ]]; do
-  CALLR=$(echo ${RANDOM} | md5sum | cut -c -6)
-  CALLED=0
-  # test to see if a file is already there by that name
-  if test ! -f "/var/www/html/said/${CALLR}"; then
-    CALLED=1
-    # the main api calling function
-    call_api;
-    break;
-  else
-    CALLED=0
-  fi
-done
+call_api;
