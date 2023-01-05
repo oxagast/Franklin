3f1b156d22910a7d11c588436ae795a78853cb5b
Marshall Lee Whittaker <marshallwhittaker@gmail.com>
2023-01-04 21:14:05 -0500

    Made some changes to filtering.

--- call_gpt3.sh
+++ call_gpt3.sh
@@ -4,21 +4,22 @@
 ## /trigger add -publics -regexp '^Franklin. (.*)' -command 'exec -o ~/call_gpt3.sh \'$1\' \'$N\''
 
 SITE="https://gpt3.oxasploits.com" # where to host the overrun
-OUT_PTH="/var/www/html"
-HARDL="-350"                       # hard limit (in bytes)
-SOFTL="120"                        # soft limit (in words)
+OUT_PTH="."
+HARDL="-350" # hard limit (in bytes)
+SOFTL="120"  # soft limit (in words)
 MODEL="text-davinci-003"
 HEAT="0.7"
 LOGF="./franklin.log"
 
 SAY="$1"
 WHO="$2"
-WHO_S=$(echo ${WHO} | tr -d '`' | tr -d '&' | tr -d "'" | tr -d ";")
+WHO_S=$(echo ${WHO} | tr -d '\`' | tr -d '&' | tr -d "'" | tr -d ";")
 # I can't decide if you're wearing me out or wearing me well
 # I just feel like I'm condemned to wear someone else's hell
 # We've only reached the third day of our seven-day binge
 
-function call_api () {
+function call_api()
+{
   APIKEY=$(<api.key)
   SRV="https://api.openai.com/v1/completions"
   if [[ $(grep "${WHO_S}" block.list | wc -l) -eq 0 ]]; then
@@ -28,33 +29,39 @@ function call_api () {
     RES=$(curl -s ${SRV} -H "Content-Type: application/json" -H "Authorization: Bearer ${APIKEY}" \
       -d "{\"model\": \"${MODEL}\",\"prompt\": \"${SAY}\",\"temperature\": ${HEAT},\"max_tokens\": \
       ${SOFTL},\"top_p\": 1,\"frequency_penalty\": 0,\"presence_penalty\": 0}")
-    # sanitizes input
-    SANI_RES=$(echo "${RES}" | tr -d "\'" | tr -d "\`" | tr -d "\;" | tr -d '\&')
-    CALLR=$(echo "${SANI_RES}" | md5sum | cut -c -8)
-    # json extraction and formatting
-    echo "${SAY}" >${OUT_PTH}/said/${CALLR}
-    echo >>${OUT_PTH}/said/${CALLR}
-    OUT_TXT=$(echo ${SANI_RES} | jq -r '.choices[].text' | sed -e 's|\\n||g' | tr -d '\n')
-    echo "${OUT_TXT}" >>${OUT_PTH}/said/${CALLR}
-    # add it to the webserver dir and say/log its response
-    SAID=$(echo "${OUT_TXT}" | sed -e 's/[^\.]$/.../' | cut -c ${HARDL})
-    echo "${SAID} ${SITE}/said/${CALLR}" | tee -a ${LOGF}
+    if [ $? -eq 0 ]; then
+      # sanitizes input
+      SANI_RES=$(echo "${RES}" | tr -d "'" | tr -d "\`" | tr -d ";" | tr -d '&')
+      CALLR=$(echo "${SANI_RES}" | md5sum | cut -c -8)
+      # json extraction and formatting
+      echo "${SAY}" >${OUT_PTH}/said/${CALLR}
+      echo >>${OUT_PTH}/said/${CALLR}
+      OUT_TXT=$(echo ${SANI_RES} | jq -r '.choices[].text' 2>/dev/null)
+      if [ $? -eq 0 ]; then
+        echo "${OUT_TXT}" | sed -e 's|\\n||g' | tr -d '\n' >>${OUT_PTH}/said/${CALLR}
+        # add it to the webserver dir and say/log its response
+        SAID=$(echo "${OUT_TXT}" | sed -e 's|\\n||g' | tr -d '\n' | sed -e 's/[^\.]$/.../' | cut -c ${HARDL})
+        echo "${SAID} ${SITE}/said/${CALLR}" | tee -a ${LOGF}
+      else
+        echo "Error parsing json."
+      fi
+    else
+      echo "Did not get response from server."
+    fi
   else
     echo "You cannot use that command."
   fi
 }
 
 if [ -d ${OUT_PTH}/said ]; then
-  call_api;
+  call_api
 else
-  >&2 echo "Running setup routine..";
+  echo >&2 "Running setup routine.."
   mkdir -p ~/.irssi/scripts/ || die $?
-  cp trigger.pl ~/.irssi/scripts/;
+  cp trigger.pl ~/.irssi/scripts/
   mkdir -p ${OUT_PTH}/said || die $?
   touch ${LOGF}
   touch api.key
-  >&2 echo "Setup complete..."
-  call_api;
+  echo >&2 "Setup complete..."
+  call_api
 fi
-
-
