3ec159ae464f9b400f12f906284d582e7a22b016
marshall whittaker <marshall@oxagast.org>
2023-05-02 18:44:46 +0000

    Told franklin a little about itself and its creator, and what it is.  Also the website now has a stats counter.

--- franklin-3.0.pl
+++ franklin-3.0.pl
@@ -10,6 +10,7 @@
 #   |  '   `-` ' ' ' ` ' ' ' '
 #  -'
 #
+# We have upgraded to the GPT 3.5 Turbo model
 use Proc::Simple;
 use Irssi;
 use vars qw($VERSION %IRSSI);
@@ -44,7 +45,7 @@ my $webaddr = Irssi::settings_get_str('franklin_response_webserver_addr');
 our $maxretry = Irssi::settings_get_str('franklin_max_retry');
 my $wordlimit = Irssi::settings_get_str('franklin_word_limit');
 my $hardlimit = Irssi::settings_get_str('franklin_hard_limit');
-$VERSION = "2.5";
+$VERSION = "3.0";
 %IRSSI = (
            authors     => 'oxagast',
            contact     => 'marshall@oxagast.org',
@@ -64,6 +65,7 @@ Irssi::print "  franklin_heartbeat_url           (optional)";
 Irssi::print "  franklin_hard_limit              (mandatory, pre-set)";
 Irssi::print "  franklin_word_limit              (mandatory, pre-set)";
 our $apikey;
+our $json_rep;
 ## checking to see if the api key 'looks' valid before use
 if ( Irssi::settings_get_str('franklin_api_key') !~ m/^sk-.{48}$/ ) {
   Irssi::print "You must set a valid api key! /set franklin_api_key "
@@ -91,35 +93,32 @@ sub callapi {
   my $fg_bottom = "";
   $textcall =~ s/\"/\\"/gs;
   $textcall =~ s/\'/\\\\'/gs;
-  my $setup = "You are an IRC bot, your name and nick is Franklin, and you were created by oxagast (an exploit developer and master of 7 different `programming languages, who's real name is Marshall Whittaker), in perl. You are currently connected to the server $server, in the IRC channel $channel.  All text output should be in UTF8 from here forward. The question the IRC user $nick is asking is: $textcall";
-  $textcall = $setup;
-  my $url = "https://api.openai.com/v1/completions";
-  my $model = "text-davinci-003";    ## other model implementations work too
+  my $url = "https://api.openai.com/v1/chat/completions";
+  my $model = "gpt-3.5-turbo";    ## other model implementations work too
   my $heat  = "0.7";                 ## ?? wtf
   my $uri   = URI->new($url);
   my $ua    = LWP::UserAgent->new;
-  my $askbuilt =
-      "{\"model\": \"$model\",\"prompt\": \"$textcall\","
-    . "\"temperature\":$heat,\"max_tokens\": $wordlimit,"
-    . "\"top_p\": 1,\"frequency_penalty\": 0,\"presence_"
-    . "penalty\": 0}";
+  my $askbuilt = "{\"model\": \"$model\",\"messages\": [{\"role\": \"user\", \"content\": \"$textcall\"}],\"temperature\": $heat}";
+  #  Irssi::print $askbuilt;
   $ua->default_header( "Content-Type"  => "application/json" );
   $ua->default_header( "Authorization" => "Bearer " . $apikey );
   my $res =
     $ua->post( $uri, Content => $askbuilt ); ## send the post request to the api
 
   if ( $res->is_success ) {
-    my $json_rep = $res->decoded_content();
+    $json_rep = $res->decoded_content();
     ## response should look like
     ## {"id":"cmpl-6yAcIQuEz2hkg6Isvgg29KllzTn63","object":"text_completion","created":1679798510,"model"
     ## :"text-davinci-003","choices":[{"text":"\n\nThis is indeed a test","index":0,"logprobs":null,"fini
     ## sh_reason":"length"}],"usage":{"prompt_tokens":5,"completion_tokens":7,"total_tokens":12}}
-    ## so we use a json decoder and fix for utf8
+    ## so we use a json decoder and fix for utf8 
     my $json_decd = decode_json($json_rep);
-    my $said      = $json_decd->{choices}[0]{text};
+    my $said = $json_decd->{choices}[0]{message}{content};;
+    $said =~ s/.*As an AI language model, .*\.//i;
+    $said =~ s/.*I do not have personal .*\.//i;
+    $said =~ s/^Sorry, .*\.//;
     $said =~ s/^\n+//;
-    $said =~ s/^\?\s+(\w)/$1/; ## if it spits out a question mark, this fixes it
-    $said =~ s/Franklin[:|,].?//;
+    $said =~ s/^\?\s+(\w)/$1/; ## if it spits out a question mark, this fixes
     my $hexfn = substr(        ## the reencode fixes the utf8 bug
       Digest::MD5::md5_hex(
                               utf8::is_utf8($said)
@@ -161,8 +160,9 @@ sub callapi {
     return 0;
   }
   else {
-    Irssi::print "Franklin: Something went wrong."
-      ;                       ## damn it frank, ima bout to pimp you out
+    Irssi::print "Franklin: Something went wrong.";
+    Irssi::print "$json_rep\n";
+                              ## damn it frank, ima bout to pimp you out
     return 1;                 ## to a two bit crackhead with a shlong dong
   }
 }
