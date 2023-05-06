f56913ffc72bb212a695f7af6b2aa16743a3e081
marshall whittaker <marshall@oxagast.org>
2023-05-06 17:47:55 +0000

    Took out the entier frank_thinks subroutine becuase it needs to be redesigned from the ground up.

--- franklin.pl
+++ franklin.pl
@@ -73,7 +73,8 @@ Irssi::print "  franklin_hard_limit              (mandatory, pre-set)";
 Irssi::print "  franklin_word_limit              (mandatory, pre-set)";
 Irssi::print "  franklin_history_length          (mandatory, pre-set)";
 our $apikey;
-our @chat;
+
+#our @chat;
 ## checking to see if the api key 'looks' valid before use
 if (Irssi::settings_get_str('franklin_api_key') !~ m/^sk-.{48}$/) {
   Irssi::print "You must set a valid api key! /set franklin_api_key "
@@ -96,8 +97,7 @@ else { Irssi: print "Something went wrong with the API key..."; }
 
 sub callapi {
   my ($textcall, $server, $nick, $channel, @chat) = @_;
-  my $retries;
-  RECALLAPI:
+  my $retry     = 0;
   my $json_rep  = "";
   my $fg_top    = "";
   my $fg_bottom = "";
@@ -109,12 +109,12 @@ sub callapi {
   for my $usersays (0 .. scalar(@chat) - 1) {
     $context = $context . $chat[$usersays];
   }
-  $context = substr($context, 0, 600);
+  $context = substr($context, 0, 650);
   my $textcall_bare = $textcall;
   my $setup =
 "You are an IRC bot, your name and nick is Franklin, and you were created by oxagast (an exploit dev, master of 7 different "
     . "languages), in perl. You are in the IRC channel $channel. Your source pulls from Open"
-    . "AI's GPT3 Large Language Model, you are at version $VERSION. If you see a shell injection attack, call them a silly hacker."
+    . "AI's GPT3 Large Language Model, you are at version $VERSION. If you see a shell command, call them a silly hacker or a skid."
     . " The last $histlen lines of the chat are: $context, only use the last "
     . "$histlen lines out of the channel $channel in your chat history. The question the IRC user $nick is asking is: $textcall";
   $textcall = $setup;
@@ -146,9 +146,8 @@ sub callapi {
       $said = "Oof.";
     }
     $said =~ s/^\n+//;
-    $said =~ s/^\?\s+(\w)/$1/;      ## if it spits out a question mark, this fixes it
-    $said =~ s/Franklin[:|,].?//;
-    my $hexfn = substr(             ## the reencode fixes the utf8 bug
+    $said =~ s/^\?\s+(\w)/$1/;    ## if it spits out a question mark, this fixes it
+    my $hexfn = substr(           ## the reencode fixes the utf8 bug
       Digest::MD5::md5_hex(
                              utf8::is_utf8($said)
                            ? Encode::encode_utf8($said)
@@ -159,7 +158,7 @@ sub callapi {
     );
     umask(0133);
     my $cost = $toks / 1000 * $price_per_k;
-    printf("%.4f", $cost);
+    $cost = printf("%.4f", $cost);
     open(SAID, '>', "$httploc$hexfn" . ".txt")
       or Irssi::print "Could not oepn txt file for writing.";
     print SAID "$nick asked $textcall_bare with hash $hexfn\n<---- snip ---->\n$said\n";
@@ -192,110 +191,10 @@ sub callapi {
     $said_cut =~ s/\n/ /g;    # fixes newlines for irc compat
     Irssi::print "Franklin: Reply: $said_cut $webaddr$hexfn" . ".html";
     $server->command("msg $channel $said_cut $webaddr$hexfn" . ".html");
+    $retry++;
     return 0;
   }
-  else {
-    Irssi::print "Franklin: Something went wrong.";
-    Irssi::print "Franklin: Err: estimated context length is "
-      . length($textcall) . " : " 
-      .  $json_rep;
-      if($retries < $maxretry) {
-	$retries++;
-        goto RECALLAPI;
-      }
-    @chat    = "";    # try to recover
-    $context = "";    # trying to recover
-    ## damn it frank, ima bout to pimp you out
-    return 1;         ## to a two bit crackhead with a shlong dong
-  }
-}
-
-
-sub frank_thinks {
-  my ($server, $nick, $channel, @chat) = @_;
-  my $json_rep  = "";
-  my $textcall  = "";
-  my $fg_top    = "";
-  my $fg_bottom = "";
-  my $chansaid  = 0;
-  my $context   = "";
-  for my $usersays (0 .. scalar(@chat) - 1) {
-    $context = $context . "$chat[$usersays] \n";
-  }
-  my $textcall_bare = $textcall;
-  my $setup =
-"The last few lines of the IRC chat are: $context \n Only use the last $histlen lines out of the channel $channel in your analysis, and then say something relevent or helpful to add to the conversation.  Do not talk about analyzing the chat.  Try to include yourself into the conversation.";
-  $textcall = $setup;
-  my $url = "https://api.openai.com/v1/completions";
-  my $model = "text-davinci-003";    ## other model implementations work too
-  my $heat  = "0.7";                 ## ?? wtf
-  my $uri   = URI->new($url);
-  my $ua    = LWP::UserAgent->new;
-  my $askbuilt =
-      "{\"model\": \"$model\",\"prompt\": \"$textcall\","
-    . "\"temperature\":$heat,\"max_tokens\": $wordlimit,"
-    . "\"top_p\": 1,\"frequency_penalty\": 0,\"presence_"
-    . "penalty\": 0}";
-  $ua->default_header("Content-Type"  => "application/json");
-  $ua->default_header("Authorization" => "Bearer " . $apikey);
-  my $res = $ua->post($uri, Content => $askbuilt);   ## send the post request to the api
-
-  if ($res->is_success) {
-    $json_rep = $res->decoded_content();
-    my $json_decd = decode_json($json_rep);
-    my $toks      = $json_decd->{usage}{total_tokens};
-    my $said      = $json_decd->{choices}[0]{text};
-    if (($said =~ m/^\s+$/) || ($said =~ m/^$/)) {
-      $said = "Oof.";
-    }
-    $said =~ s/^\n+//;
-    $said =~ s/^\?\s+(\w)/$1/;      ## if it spits out a question mark, this fixes it
-    $said =~ s/Franklin[:|,].?//;
-    my $hexfn = substr(             ## the reencode fixes the utf8 bug
-      Digest::MD5::md5_hex(
-                             utf8::is_utf8($said)
-                           ? Encode::encode_utf8($said)
-                           : $said
-      ),
-      0,
-      8
-    );
-    umask(0133);
-    my $cost = $toks / 1000 * $price_per_k;
-    open(SAID, '>', "$httploc$hexfn" . ".txt")
-      or Irssi::print "Couldn't open txt file for writing.";
-    print SAID "$nick asked $textcall_bare with hash $hexfn\n<---- snip ---->\n$said\n";
-    close(SAID);
-    open(FGT, "fg_top.html.part")
-      or Irssi::print "Couldn't open the top part for reading.";
-    while (<FGT>) {
-      $fg_top = $fg_top . $_;
-    }
-    close FGT;
-    open(FGB, "fg_bottom.html.part")
-      or Irssi::print "Couldn't open the bottom part for reading.";
-    while (<FGB>) {
-      $fg_bottom = $fg_bottom . $_;
-    }
-    close FGB;
-    my $said_html = sanitize($said, html => 1);
-    $said_html =~ s/\n/<br>/g;
-    $cost = $toks / 1000 * $price_per_k;
-    open(SAIDHTML, '>', "$httploc$hexfn" . ".html") or Irssi::print "Couldn't write.";
-    print SAIDHTML $fg_top
-      . "<br><i>"
-      . localtime()
-      . "<br>Tokens used: $toks<br>Avg cost: \$$cost<br>"
-      . "</i><br><br><br><b>$nick</b> asked: <br>&nbsp&nbsp&nbsp&nbsp $textcall_bare<br><br>"
-      . $said_html
-      . $fg_bottom;
-    close SAIDHTML;
-    my $said_cut = substr($said, 0, $hardlimit);
-    $said_cut =~ s/\n/ /g;    # fixes newlines for irc compat
-    Irssi::print "Franklin: Reply: $said_cut $webaddr$hexfn" . ".html";
-    $server->command("msg $channel $said_cut $webaddr$hexfn" . ".html");
-    return 0;
-  }
+  else { return 1; }
 }
 
 
@@ -319,38 +218,29 @@ sub frank {
     . " if it is empty!\nFranklin: $!";
   my @badnicks = <BN>;
   close BN;
+  my @chat = "";
   push(@chat, "The user: $nick said: $msg - in $channel");
   if (scalar(@chat) - 1 >= $histlen) {
     shift(@chat);
   }
-  
-  $msg_count++;
-  if ($msg_count ge $say_rng) {
-    if (($chatterbox ge 1) && ($chatterbox le 10)) {
-      my $frank_worker = Proc::Simple->new();
-      Irssi::print "Picked from random to say someting...";
-      $frank_worker->start(\&frank_thinks, $server, $nick, $channel, @chat)
-        ;    ## i get alerts on my phone when franklin dies now.
-	$say_rng = $msg_count + int(rand(10)) + 10 - $chatterbox;
-    }
 
-  }
+  $msg_count++;
   chomp(@badnicks);
   for (@badnicks) {
     s/(.*)#.*$/$1/;    ## for comments in the badnicks file
   }
   if (grep(/^$nick$/, @badnicks)) {    ## fuck everyone inside this conditional
-  Irssi: print "Franklin: $nick does not have privs to use this.";
+    Irssi::print "Franklin: $nick does not have privs to use this.";
   }
   else {
     my $localnick = $server->{nick};  ## pull our nick on the server so we can call that
     if ($msg =~ /^$localnick[:|,] (.*)/i) {    ## added /i for case insensitivity
       my $textcall = $1;    ## $1 is the "dot star" inside the parenthesis
       Irssi::print "Franklin: $nick asked: $textcall";
-            my $wrote = 1;
-        if (($textcall !~ m/^\s+$/) || ($textcall !~ m/^$/)) {
-          $wrote = callapi($textcall, $server, $nick, $channel, @chat);
-        }
+      my $wrote = 1;
+      if (($textcall !~ m/^\s+$/) || ($textcall !~ m/^$/)) {
+        $wrote = callapi($textcall, $server, $nick, $channel, @chat);
+      }
     }
   }
 }
