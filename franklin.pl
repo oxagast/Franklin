7a7c02ea9b7d7c4ab8b048308d2128ba6f639df2
Marshall Whittaker <marshall@oxagast.org>
2023-08-08 21:04:40 -0400

    Maybe it was in the wrong spot.

--- franklin.pl
+++ franklin.pl
@@ -185,7 +185,7 @@ m!(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&
 }
 
 sub asshat {
-  my ($textcall, $server, $nick, $channel) = @_;
+  my ($server, $nick, $channel, $textcall) = @_;
   my $cmn = $server->channel_find($channel)->nick_find($server->{nick});
   if (($cmn->{op} eq 1) || ($cmn->{halfop} eq 1)) {
   my $setup = "Rate the comment $textcall on a scale from 1 to 10 on how much of an asshole the user is being, format your response as just the number alone on one line.";
@@ -216,7 +216,7 @@ sub asshat {
 }
 
 sub callapi {
-  my ($textcall, $server, $nick, $channel) = @_;
+  my ($server, $nick, $channel, $textcall) = @_;
   my $retry    = 0;
   my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
   my @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
