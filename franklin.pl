e33555c161143301027b2d5cddff0134a5373c1c
Marshall Lee Whittaker <marshall@oxagast.org>
2023-08-19 15:32:41 -0400

    oops

--- franklin.pl
+++ franklin.pl
@@ -376,7 +376,7 @@ sub frank {
     s/(.*)#.*$/$1/;    ## for comments in the badnicks file
   }
   if (($msg =~ /quack!/i) || ($msg =~ /flap flap!/i)) && ($nick == "JARVIS") {
-    if (int(rand(10)) eq 1) {
+    if (int(rand(10) eq 1) {
       sleep(0.950);
       $server->command("msg $channel .bang TXID:0000008b");
     }
