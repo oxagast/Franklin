2581ea952e2dff9656f5ed6d67e29470371738ee
marshall whittaker <marshall@oxagast.org>
2023-10-14 20:20:07 -0400

    Rewrite of the website, lots of changes.

---
+++ scripts/changedate.sh
@@ -0,0 +1,2 @@
+D=$(date +"%a %b %d, %Y");
+echo "<h5 id="date"> $D </h5>"
--- scripts/totals.sh
+++ scripts/totals.sh
@@ -1,4 +1,4 @@
-TQ=$(find /var/www/html/said -name "*.txt" -print | wc -l)
+TQ=$(find /var/www/franklin/said -name "*.txt" -print | wc -l)
 TBQ=$(numfmt --to=si --format %.2f $TQ)
-DQ=$(find /var/www/html/said -name "*.txt" -mtime -1 -print | wc -l)
+DQ=$(find /var/www/franklin/said -name "*.txt" -mtime -1 -print | wc -l)
 echo "<p>There have been <i> $DQ </i> queries in the last 24 hours, and <i> $TBQ </i> queries to Franklin since initiation on this server. Updates every 10 minutes.</p>"
