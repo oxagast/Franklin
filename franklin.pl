67e1d4f27fd381e105f228e1776af1d7c7ed3d25
Marshall Lee Whittaker <oxagast@oxasploits.com>
2023-11-27 00:21:28 -0500

    Better to rewrite the @ array notation as a scalar ($).

--- franklin.pl
+++ franklin.pl
@@ -96,9 +96,9 @@ if (Irssi::settings_get_str('franklin_api_key') =~ m/^sk-.{48}$/) {
 else { Irssi: print "Something went wrong with the API key..."; }
 
 my @chanlst;
-@chanlst[0] = @txidchans[0] . " " . @txidchans[1] . " " . @txidchans[2];
-@chanlst[1] = @txidchans[3] . " " . @txidchans[4] . " " . @txidchans[5];
-@chanlst[2] = @txidchans[6] . " " . @txidchans[7] . " " . @txidchans[8];
+$chanlst[0] = $txidchans[0] . " " . $txidchans[1] . " " . $txidchans[2];
+$chanlst[1] = $txidchans[3] . " " . $txidchans[4] . " " . $txidchans[5];
+$chanlst[2] = $txidchans[6] . " " . $txidchans[7] . " " . $txidchans[8];
 my $apifirstp = substr($apikey, 0,  16);
 my $apilastp  = substr($apikey, 40, 49);
 Irssi::print "";
