e1034db9247458cb2af029f6ad93d6c342e64cdb
Marshall Lee Whittaker <oxagast@oxasploits.com>
2023-11-27 01:09:07 -0500

    Revert "Check if  is defined before checking if it equals nicks becase we should."
    This reverts commit ef5579687701e66edf4e8150942ff0ef858b1af7.

--- franklin.pl
+++ franklin.pl
@@ -287,7 +287,7 @@ sub callapi {
   else {
     $modstat = " not a channel";
   }
-  if (($cmn) && ($cmn ne $nick)) {
+  if ($cmn ne $nick) {
     my $textcall_bare = $textcall;
     my $dcp;
     if (($page) && (length($page) >= 20)) {
